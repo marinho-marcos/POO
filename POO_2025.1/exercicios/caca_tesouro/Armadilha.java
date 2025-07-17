93f9353f65129a107cef7acb1df404acc726a851
marinho-marcos <110265011+marinho-marcos@users.noreply.github.com>
2025-07-17 12:10:22 -0300

    Add files via upload

---
+++ POO_2025.1/exercicios/caca_tesouro/Armadilha.java
@@ -0,0 +1,9 @@
+public class Armadilha extends ElementoTabuleiro{
+    public int interagir(){
+        return -2;
+    }
+
+    public String simbolo(){
+        return "💣";
+    }
+}
---
+++ POO_2025.1/exercicios/caca_tesouro/Jogo.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Jogo {
+    ElementoTabuleiro[][] tabuleiro;
+
+    public Jogo(){
+        tabuleiro = new ElementoTabuleiro[6][6];
+    }
+
+    
+}
